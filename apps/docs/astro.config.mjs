e777d64e315fc577918f209bf79ddf2ada3802af
Diógenes Rodrigues <37380674+dxdns@users.noreply.github.com>
2026-03-16 08:35:42 -0300

    fixes

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -29,13 +29,10 @@ export default defineConfig({
 				{
 					tag: "script",
 					content: `
-					<script>
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){dataLayer.push(arguments);}
 						gtag('js', new Date());
-
 						gtag('config', '${googleAnalyticsId}');
-					</script>
 					`
 				}
 			],
