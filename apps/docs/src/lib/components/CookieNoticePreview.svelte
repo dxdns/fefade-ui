72e0ed3f8009453bd0009de7400e2568e2086b60
dx <37380674+dxdns@users.noreply.github.com>
2026-03-26 09:48:47 -0300

    chore: cookie notice preview style

--- apps/docs/src/lib/components/CookieNoticePreview.svelte
+++ apps/docs/src/lib/components/CookieNoticePreview.svelte
@@ -19,20 +19,25 @@
 
 <Button onclick={handleOpen}>Open</Button>
 
-<Drawer {isOpen} position="bottom" style="padding: 0;">
-	<Drawer.Header style="justify-content: flex-start;">
+<Drawer
+	{isOpen}
+	position="bottom"
+	style="
+	padding: 0; 
+	display: flex; 
+	flex-direction: column; 
+	align-items: center;
+	"
+>
+	<Drawer.Header>
 		<strong>🍪 We use cookies</strong>
 	</Drawer.Header>
-	<Drawer.Content>
+	<Drawer.Content style="max-width: 500px; gap: 0.5rem;">
 		<p>
 			We use cookies to enhance your experience. By continuing to visit this
 			site you agree to our use of cookies.
 		</p>
-		<div
-			style="display: flex; justify-content: flex-end; gap: 0.5rem; align-items: baseline;"
-		>
-			<Button variant="outlined" onclick={handleClose}>Decline</Button>
-			<Button>Accept</Button>
-		</div>
+		<Button variant="outlined" onclick={handleClose}>Decline</Button>
+		<Button>Accept</Button>
 	</Drawer.Content>
 </Drawer>
